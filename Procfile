c34a20335e78b07e843b12363b5b3b8d90df5972
Narenyadav0929 <Narenyadav0929@gmail.com>
2025-11-05 10:38:04 +0530

    Adding procfile for AWS elastic bean stalk

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --chdir aws aws.application:application
+web: gunicorn -k gthread -b 127.0.0.1:5000 application:app
--- application.py
+++ application.py
@@ -114,10 +114,12 @@ def predict_datapoint():
                 ),
                 200,
             )
-    
+
+
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=False)
+    app.run(host="0.0.0.0",debug=False,port=5000)
     # app.run(host='0.0.0.0', port=8080, debug=True)
 
     
